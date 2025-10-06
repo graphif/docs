9aba7d81379c01f3e5df8bf779641286e08639fa
zty012 <z@2y.nz>
2025-10-06 12:39:37 +0800

    chore: 将文档从project-graph仓库分离

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import { createMDX } from "fumadocs-mdx/next";
+
+const withMDX = createMDX();
+
+/** @type {import('next').NextConfig} */
+const config = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  // output: "export",
+};
+
+export default withMDX(config);
