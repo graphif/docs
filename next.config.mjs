4a01a0fe3e99ba5e095a9b4fca7fdfc934775b90
zty012 <z@2y.nz>
2026-03-21 00:42:43 +0800

    feat: remove donation page and client components

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,20 @@ const withMDX = createMDX();
 const config = {
   reactStrictMode: true,
   // output: "export",
+  async redirects() {
+    return [
+      {
+        source: "/:lang(en|zh-CN)/donate",
+        destination: "https://api.graphif.dev/ext/donate",
+        permanent: true,
+      },
+      {
+        source: "/donate",
+        destination: "https://api.graphif.dev/ext/donate",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
